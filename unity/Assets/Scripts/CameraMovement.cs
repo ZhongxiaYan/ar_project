ef18ccdc535b83955c43acf27393508568df6326
Zhongxia Yan <zxyan@berkeley.edu>
2017-02-14 00:33:49 -0800

    created rudimentary sequence selector code

---
+++ unity/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    float zoomSpeed = 6.0f;
+    float translateSpeed = 3.0f;
+
+	void Update() {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.Translate(zoomSpeed * scroll * transform.forward);
+        if (Input.GetKey("up")) {
+            transform.Translate(translateSpeed * transform.up);
+        }
+        if (Input.GetKey("down")) {
+            transform.Translate(translateSpeed * -transform.up);
+        }
+        if (Input.GetKey("right")) {
+            transform.Translate(translateSpeed * transform.right);
+        }
+        if (Input.GetKey("left")) {
+            transform.Translate(translateSpeed * -transform.right);
+        }
+	}
+}
---
+++ unity/Assets/Scripts/Manager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+    List<GameObject> orderList;
+    Dictionary<GameObject, Vector3> origCentroid;
+    Dictionary<GameObject, Vector3> newCentroid;
+    Vector3 baseOffset = new Vector3();
+    bool isBaseSet = false;
+
+	void Start () {
+        orderList = new List<GameObject>();
+        origCentroid = new Dictionary<GameObject, Vector3>();
+        newCentroid = new Dictionary<GameObject, Vector3>();
+
+        GameObject obj = OBJLoader.LoadOBJFile("Assets/LEGO_CAR_B1_small.obj");
+        foreach (Transform childTrans in obj.transform) {
+            GameObject child = childTrans.gameObject;
+            Mesh mesh = child.GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+            Vector3 centroid = new Vector3();
+            foreach (Vector3 vertex in vertices) {
+                centroid += vertex;
+            }
+            centroid /= vertices.Length;
+            int i = 0;
+            foreach (Vector3 vertex in vertices) {
+                vertices[i] += centroid;
+                i++;
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+
+            origCentroid[child] = centroid;
+            newCentroid[child] = 2 * centroid;
+            child.AddComponent<MeshCollider>();
+            child.AddComponent<MouseEvent>().manager = this;
+        }
+	}
+
+    public void AddToQueue(GameObject child) {
+        orderList.Add(child);
+        if (!isBaseSet) {
+            baseOffset = newCentroid[child] - origCentroid[child];
+            isBaseSet = true;
+            return;
+        }
+        Vector3 offset = origCentroid[child] - newCentroid[child] + baseOffset;
+        Mesh mesh = child.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        int i = 0;
+        foreach (Vector3 vertex in vertices) {
+            vertices[i] += offset;
+            i++;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+    }
+
+}
