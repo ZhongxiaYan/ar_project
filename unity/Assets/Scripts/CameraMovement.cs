493b715063a4106c0486d143d2772376af7c4090
Zhongxia Yan <zxyan@berkeley.edu>
2017-02-20 00:34:26 -0800

    added new colors for highlight / select and rotation options for selected and unselected

--- unity/Assets/Scripts/CameraMovement.cs
+++ unity/Assets/Scripts/CameraMovement.cs
@@ -3,23 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
-    float zoomSpeed = 6.0f;
-    float translateSpeed = 3.0f;
+    float zoomSpeed = 200f;
+    float translateSpeed = 100f;
+    float turnSpeed = 100f;
 
-	void Update() {
+    Manager manager = null;
+    ObjectSelector lastTargetSelector = null;
+    Vector3 target;
+
+    void Start() {
+        GameObject managerObject = GameObject.Find("Manager");
+        manager = managerObject.GetComponent<Manager>();
+    }
+
+	void LateUpdate() {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        transform.Translate(zoomSpeed * scroll * transform.forward);
-        if (Input.GetKey("up")) {
-            transform.Translate(translateSpeed * transform.up);
-        }
-        if (Input.GetKey("down")) {
-            transform.Translate(translateSpeed * -transform.up);
-        }
-        if (Input.GetKey("right")) {
-            transform.Translate(translateSpeed * transform.right);
-        }
-        if (Input.GetKey("left")) {
-            transform.Translate(translateSpeed * -transform.right);
+        transform.Translate(zoomSpeed * Time.deltaTime * scroll * Vector3.forward);
+        if (Input.GetKey(KeyCode.LeftControl)) {
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                transform.Translate(translateSpeed * Time.deltaTime * Vector3.up);
+            }
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                transform.Translate(translateSpeed * Time.deltaTime * -Vector3.up);
+            }
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                transform.Translate(translateSpeed * Time.deltaTime * Vector3.right);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                transform.Translate(translateSpeed * Time.deltaTime * -Vector3.right);
+            }
+        } else {
+            if (manager.selectedChildSelector == null) {
+                if (Input.GetKey(KeyCode.UpArrow)) {
+                    transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(KeyCode.DownArrow)) {
+                    transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(KeyCode.Period)) {
+                    transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(KeyCode.Comma)) {
+                    transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+                }
+                lastTargetSelector = null;
+            } else {
+                if (lastTargetSelector != manager.selectedChildSelector) {
+                    lastTargetSelector = manager.selectedChildSelector;
+                    target = lastTargetSelector.gameObject.transform.TransformPoint(manager.selectedChildSelector.centroid);
+                    transform.LookAt(target);
+                }
+                if (Input.GetKey(KeyCode.UpArrow)) {
+                    transform.RotateAround(target, Vector3.right, turnSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(KeyCode.DownArrow)) {
+                    transform.RotateAround(target, Vector3.right, -turnSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(KeyCode.Period)) {
+                    transform.RotateAround(target, Vector3.up, turnSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(KeyCode.Comma)) {
+                    transform.RotateAround(target, Vector3.up, -turnSpeed * Time.deltaTime);
+                }
+            }
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+            }
         }
 	}
 }
---
+++ unity/Assets/Scripts/ObjectSelector.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelector : MonoBehaviour {
+    bool isHover = false;
+    bool isAddedToQueue = false;
+
+    static Shader standardColor = null;
+    static Shader hoverColor = null;
+    static Shader pivotColor = null;
+    Material objMaterial = null;
+
+    public Manager manager = null;
+    public Vector3 centroid;
+
+    void Start() {
+        standardColor = Shader.Find("Standard");
+        hoverColor = Shader.Find("Solid Yellow");
+        pivotColor = Shader.Find("Solid Blue");
+        objMaterial = gameObject.GetComponent<Renderer>().material;
+    }
+
+    public void UpdateColor() {
+        if (isAddedToQueue) {
+            objMaterial.shader = standardColor;
+        } else if (manager.selectedChildSelector == this) {
+            objMaterial.shader = pivotColor;
+        } else if (isHover) {
+            objMaterial.shader = hoverColor;
+        } else {
+            objMaterial.shader = standardColor;
+        }
+    }
+
+    void OnMouseEnter() {
+        isHover = true;
+        UpdateColor();
+    }
+
+    void OnMouseOver() {
+        if (Input.GetMouseButtonDown(2)) {
+            manager.SetSelected(this);
+            UpdateColor();
+        }
+    }
+
+    void OnMouseExit() {
+        isHover = false;
+        UpdateColor();
+    }
+
+    void OnMouseUpAsButton() {
+        manager.AddToQueue(this);
+        isAddedToQueue = true;
+        UpdateColor();
+    }
+}
